d7d66027457c69382535eb6fb35e8dadafa95d03
alat414 <gustavo.alatriste@g.austincc.edu>
2025-09-19 16:02:14 -0500

    re-implementing base string and entity classes

--- main.cpp
+++ main.cpp
@@ -1,161 +1,37 @@
 #include <iostream>
-#include <string.h>
-#include <cstdint>
-/*
-int& GetValue()
-{
-    static int value = 17;
-    return value;
-}
+#include <cstring>
 
-int main()
+class String
 {
-    int i = GetValue();
-    GetValue() = 7;
-}
+    public:
+        String() = default;
+        String(const char* string)
+        {
+            printf("Created!\n");
+            m_Size = strlen(string);
+            m_Data = new char[m_Size];
+            memcpy(m_Data, string, m_Size);
+        }
+        ~String()
+        {
+            delete m_Data;
+
+        }
+    private:
     
+        char* m_Data;
+        uint32_t m_Size;
 
-void PrintName(const std:: string& name)
-{
-    std::cout << "[lvalue] " << name << std::endl;
-}
-void PrintName(std:: string&& name)
-{
-    std::cout << "[rvalue] " << name << std::endl;
-}
-int main() 
-{
-    std::string firstName = "Gustavo";
-    std::string lastName = "Alatriste";
+};
 
-    std::string fullName = firstName + lastName;
-
-    PrintName(fullName);
-    PrintName(firstName + lastName);
-
-    std::cin.get();
-    
-}  
-
-*/
 class Entity
 {
     public:
         Entity(const String& name)
             : m_Name(name)
         {
-
-        }
-        Entity(String && name)
-            : m_Name(std::move(name))
-        {
-
-        }
-        void PrintName()
-        {
-            m_Name.Print();
         }
     private:
         String m_Name;
 
 };
-
-
-class String
-{
-public:
-    String() : m_Data(nullptr), m_Size(0) {}
-    
-    String(const char* string)
-    {
-        printf("Created!\n");
-        m_Size = strlen(string);
-        m_Data = new char[m_Size + 1];
-        memcpy(m_Data, string, m_Size);
-        m_Data[m_Size] = '\0';
-    }
-
-    String(const String& other)
-    {
-        printf("Copied!\n");
-        m_Size = other.m_Size;
-        m_Data = new char[m_Size + 1];
-        memcpy(m_Data, other.m_Data, m_Size + 1);
-    }
-    
-    String(String&& other) noexcept
-    {
-        printf("Moved!\n");
-        m_Size = other.m_Size;
-        m_Data = other.m_Data;
-       
-        other.m_Size = 0;
-        other.m_Data = nullptr;
-    }
-
-    String& operator=(const String& other)
-    {
-        printf("Copied!\n");
-        if (this != &other)
-        {
-            delete[] m_Data;
-            m_Size = other.m_Size;
-            m_Data = new char[m_Size + 1];
-            memcpy(m_Data, other.m_Data, m_Size + 1);
-        }
-        return *this;
-    }
-
-    String& operator=(String&& other) noexcept
-    {
-        printf("Moved!\n");
-        if (this != &other)
-        {
-            delete[] m_Data;
-            m_Size = other.m_Size;
-            m_Data = other.m_Data;
-       
-            other.m_Size = 0;
-            other.m_Data = nullptr;
-        }
-        return *this;
-    }
-
-    ~String()
-    {
-        printf("Deleted!\n");
-        delete[] m_Data;
-    }
-
-    void Print()
-    {
-        if (m_Data)
-        {
-            for (uint32_t i = 0; i < m_Size; i++)
-            {
-                printf("%c", m_Data[i]);
-            }
-        }
-        printf("\n");
-    }
-
-private:
-    char* m_Data;
-    uint32_t m_Size;
-};
-
-int main()
-{
-    String apple = "Apple";
-    String dest;
-    apple.Print();
-    dest.Print();
-
-    dest = std::move(apple);
-    apple.Print();
-    dest.Print();
-
-    std::cin.get();
-}
-
-// Entity class remains the same...
